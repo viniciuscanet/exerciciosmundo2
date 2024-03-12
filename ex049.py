944aeba2d4bb59fbc359484326c2e70b0bc96a2a
viniciuscanet <viniciusdcanet@ufpr.br>
2024-03-11 23:03:28 -0300

    Modificando para 166

--- ex049.py
+++ ex049.py
@@ -1,5 +1,5 @@
 #construindo uma tabuada
 
 x = int(input('Digite no número que deseja saber a tabuada: '))
-for n in range (1,100):
+for n in range (1,166):
     print('{} X {} = {}'.format(n, x, (n*x)))
