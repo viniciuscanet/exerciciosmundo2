df5623590d298a2b21fe1df554bda7f2d0012181
viniciuscanet <viniciusdcanet@ufpr.br>
2024-03-11 23:13:09 -0300

    Testar a branch do universo paralelo

--- ex049.py
+++ ex049.py
@@ -3,7 +3,7 @@
 print('Teste do merge')
 
 x = int(input('Digite no número que deseja saber a tabuada: '))
-for n in range (1,166):
+for n in range (1,321):
     print('{} X {} = {}'.format(n, x, (n*x)))
 
 
